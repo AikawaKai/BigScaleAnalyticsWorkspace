75a0e50f60cf12325610b5d66e7cc26206a04dd5
Marco Odore <m.odore@gotosrl.it>
2016-02-20 17:56:31 +0100

    update

--- Clustering/CurseOfDimensionality.py
+++ Clustering/CurseOfDimensionality.py
@@ -3,34 +3,37 @@ from math import sqrt
 from itertools import combinations
 
 
-def genRandomArray(size):
-    return [np.random.uniform(0, 1) for i in range(0, size)]
+def genRandomArray(d):
+    return [np.random.uniform(0, 1) for i in range(0, d)]
 
 
-def genSample(size, d):
-    return [genRandomArray(size) for i in range(0, size)]
+def genSample(number, d):
+    return [genRandomArray(d) for i in range(0, number)]
 
 
-def distanceTwoPoints(point1, point2, size):
-    return sqrt(sum([pow(point1[i]-point2[i], 2) for i in range(size)]))
+def distanceTwoPoints(point1, point2, d):
+    return sqrt(sum([pow(point1[i]-point2[i], 2) for i in range(d)]))
 
 
-def averageDistance(sample, size):
+def averageDistance(sample, d):
     couple = list(combinations(sample, 2))
     lenCouple = len(couple)
-    return sum([distanceTwoPoints(c1, c2, size) for c1, c2 in couple])/lenCouple
+    return sum([distanceTwoPoints(c1, c2, d) for c1, c2 in couple])/lenCouple
 
 
-def standardDeviation(sample, size):
+def standardDeviation(sample, d):
     couple = list(combinations(sample, 2))
-    average = averageDistance(sample, size)
+    average = averageDistance(sample, d)
     lenCouple = len(couple)
-    return sqrt(sum([pow(distanceTwoPoints(c1, c2, size) - average, 2) for c1, c2 in couple])/lenCouple)
+    return sqrt(sum([pow(distanceTwoPoints(c1, c2, d) - average, 2) for c1, c2 in couple])/lenCouple)
 
 
 
 
 if __name__ == '__main__':
-    sample = genSample(100, 100)
-    print(averageDistance(sample, 100))
-    print(standardDeviation(sample, 100))
+    numsample = 100
+    d = 1000
+    sample = genSample(numsample, d)
+    average = averageDistance(sample, d)
+    print("(lowerbound = 1) <= {0} <= (upperbound = sqrt(d) = {1})".format(average, sqrt(d)))
+    print("Standard Deviation: ", standardDeviation(sample, d))
