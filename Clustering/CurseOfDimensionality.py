1fde9971117f36fbb5c0f8458cd9349c37d08444
Marco Odore <m.odore@gotosrl.it>
2016-02-21 11:33:29 +0100

    added cosine distance

--- Clustering/CurseOfDimensionality.py
+++ Clustering/CurseOfDimensionality.py
@@ -1,8 +1,10 @@
 import numpy as np
 from math import sqrt
+from math import acos
+from math import degrees
 from itertools import combinations
 
-lowerbound = 0
+lowerbound = -1
 upperbound = 1
 
 
@@ -18,8 +20,18 @@ def euclidianDistance(point1, point2, d):
     return sqrt(sum([pow(point1[i]-point2[i], 2) for i in range(d)]))
 
 
-def averageDistance(distFunc, sample, d):
-    couple = list(combinations(sample, 2))
+def cosineDistance(point1, point2, d):
+    num = sum([point1[i]*point2[i] for i in range(d)])
+    den1 = sqrt(sum([pow(point1[i], 2) for i in range(d)]))
+    den2 = sqrt(sum([pow(point2[i], 2) for i in range(d)]))
+    return num / (den1 * den2)
+
+
+def combinationsSample(sample):
+    return list(combinations(sample, 2))
+
+
+def averageDistance(distFunc, couple, d):
     lenCouple = len(couple)
     distances = [distFunc(c1, c2, d) for c1, c2 in couple]
     maxdistance = max(distances)
@@ -27,24 +39,28 @@ def averageDistance(distFunc, sample, d):
     return (sum(distances)/lenCouple, maxdistance, mindistance)
 
 
-def standardDeviation(sample, d, average):
-    couple = list(combinations(sample, 2))
+def standardDeviation(couple, d, average):
     lenCouple = len(couple)
     return sqrt(sum([pow(euclidianDistance(c1, c2, d) - average, 2) for c1, c2 in couple])/lenCouple)
 
 
 if __name__ == '__main__':
     numsample = 100
-    d = 1000
+    d = 10000
+    distance = cosineDistance
     print("Uniform Distribution ({0}, {1})".format(lowerbound, upperbound))
     print("Number of sample: {0}".format(numsample))
     print("Number of dimension d: {0}".format(d))
-    print("Used distance: {0}".format(euclidianDistance.__name__))
+    print("Used distance: {0}".format(distance.__name__))
 
     sample = genSample(numsample, d)
-    (average, maxd, mind) = averageDistance(euclidianDistance, sample, d)
-    print("(lowerbound={2}) <= {0} <= (upperbound = sqrt(({2}^2)*d) = {1})".format(average, sqrt(d*pow(upperbound, 2)), pow((upperbound-lowerbound), 2)))
-    print("Standard Deviation: ", standardDeviation(sample, d, average))
+    couple = combinationsSample(sample)
+    (average, maxd, mind) = averageDistance(distance, couple, d)
+    if distance.__name__ == "euclidianDistance":
+        print("(lowerbound={2}) <= {0} <= (upperbound = sqrt(({2}^2)*d) = {1})".format(average, sqrt(d*pow(upperbound, 2)), pow((upperbound-lowerbound), 2)))
+    print("Standard Deviation: ", standardDeviation(couple, d, average))
     print("Max distance: {0}".format(maxd))
     print("Min distance: {0}".format(mind))
     print("(MaxDistance - MinDistance)/MinDistance = {}".format((maxd-mind)/mind))
+    if distance.__name__ == "cosineDistance":
+        print("Average angle: {0}".format(degrees(acos(average))))
