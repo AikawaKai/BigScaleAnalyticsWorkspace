9abc133a16daa124cb2ae4627abbf1e03476f90e
Marco Odore <m.odore@gotosrl.it>
2016-02-21 11:08:51 +0100

    euclidian distance

--- Clustering/CurseOfDimensionality.py
+++ Clustering/CurseOfDimensionality.py
@@ -14,14 +14,14 @@ def genSample(number, d):
     return [genRandomArray(d) for i in range(0, number)]
 
 
-def distanceTwoPoints(point1, point2, d):
+def euclidianDistance(point1, point2, d):
     return sqrt(sum([pow(point1[i]-point2[i], 2) for i in range(d)]))
 
 
-def averageDistance(sample, d):
+def averageDistance(distFunc, sample, d):
     couple = list(combinations(sample, 2))
     lenCouple = len(couple)
-    distances = [distanceTwoPoints(c1, c2, d) for c1, c2 in couple]
+    distances = [distFunc(c1, c2, d) for c1, c2 in couple]
     maxdistance = max(distances)
     mindistance = min(distances)
     return (sum(distances)/lenCouple, maxdistance, mindistance)
@@ -30,9 +30,7 @@ def averageDistance(sample, d):
 def standardDeviation(sample, d, average):
     couple = list(combinations(sample, 2))
     lenCouple = len(couple)
-    return sqrt(sum([pow(distanceTwoPoints(c1, c2, d) - average, 2) for c1, c2 in couple])/lenCouple)
-
-
+    return sqrt(sum([pow(euclidianDistance(c1, c2, d) - average, 2) for c1, c2 in couple])/lenCouple)
 
 
 if __name__ == '__main__':
@@ -41,9 +39,10 @@ if __name__ == '__main__':
     print("Uniform Distribution ({0}, {1})".format(lowerbound, upperbound))
     print("Number of sample: {0}".format(numsample))
     print("Number of dimension d: {0}".format(d))
+    print("Used distance: {0}".format(euclidianDistance.__name__))
 
     sample = genSample(numsample, d)
-    (average, maxd, mind) = averageDistance(sample, d)
+    (average, maxd, mind) = averageDistance(euclidianDistance, sample, d)
     print("(lowerbound={2}) <= {0} <= (upperbound = sqrt(({2}^2)*d) = {1})".format(average, sqrt(d*pow(upperbound, 2)), pow((upperbound-lowerbound), 2)))
     print("Standard Deviation: ", standardDeviation(sample, d, average))
     print("Max distance: {0}".format(maxd))
