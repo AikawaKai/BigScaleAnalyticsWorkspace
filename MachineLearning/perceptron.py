301d948df5d4972691e716da7c8f12ad73b1bb48
Marco Odore <m.odore@gotosrl.it>
2016-02-26 00:40:52 +0100

    rete neurale plottabile su solo due dimensioni

--- MachineLearning/perceptron.py
+++ MachineLearning/perceptron.py
@@ -1,23 +1,5 @@
 from bokeh.plotting import figure, output_file, show
 
-'''
-# prepare some data
-x = [1, 2, 3, 4, 5]
-y = [6, 7, 2, 4, 5]
-
-# output to static HTML file
-output_file("lines.html", title="line plot example")
-
-# create a new plot with a title and axis labels
-p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
-
-# add a line renderer with legend and line thickness
-p.line(x, y, legend="Temp.", line_width=2)
-
-# show the results
-show(p)
-'''
-
 
 def dot(v1, v2):
     return sum([v1[i]*v2[i] for i in range(len(v1))])
@@ -47,7 +29,9 @@ def perceptronR(x_y, w, lr):
 
 def classify(x_y, w):
     results = [sign(dot(x, w)) == y for x, y in x_y]
-    print(w)
+    for x, y in x_y:
+        print(dot(x, w), y)
+    print(results)
     for res in results:
         if not res:
             return False
@@ -55,8 +39,8 @@ def classify(x_y, w):
 
 
 if __name__ == '__main__':
-    listOfPositivePoint = [([5, 6, 1], +1), ([6, 6, 1], +1), ([5, 7, 1], +1), ([7, 7, 1], +1)]
-    listOfNegativePoint = [([3, 2, 1], -1), ([3, 4, 1], -1), ([2, 0, 1], -1)]
+    listOfPositivePoint = [([6, 15, 1], +1), ([5, 6, 1], +1), ([6, 6, 1], +1), ([5, 7, 1], +1), ([7, 7, 1], +1)]
+    listOfNegativePoint = [([-5, 4, 1], -1),([1, 1, 1], -1), ([3, 2, 1], -1), ([3, 4, 1], -1), ([2, 0, 1], -1)]
     x_y = listOfNegativePoint + listOfPositivePoint
     print(x_y)
     w = [0, 0, 0]
@@ -70,8 +54,8 @@ if __name__ == '__main__':
     y2 = [x[1] for x, y in listOfNegativePoint]
     x3 = -15
     x4 = 15
-    y3 = -(w[0]/w[1])*(x3) - w[2]
-    y4 = -(w[0]/w[1])*(x4) - w[2]
+    y3 = -(x3 * (w[0]/w[1])) - (w[2]/w[1])
+    y4 = -(x4 * (w[0]/w[1])) - (w[2]/w[1])
     x = [x3, x4]
     y = [y3, y4]
     output_file("line.html")
@@ -80,7 +64,7 @@ if __name__ == '__main__':
     # add a circle renderer with a size, color, and alpha
     p.circle(x1, y1, size=5, color="navy", alpha=0.5)
     p.circle(x2, y2, size=5, color="red", alpha=0.5)
-    p.line(x, y, legend="Temp.", line_width=2)
+    p.line(x, y, legend="hyperplane", line_width=1)
 
     # show the results
     show(p)
