8901a5283c3405834d5064b36755321ca2f8ac64
Marco Odore <m.odore@gotosrl.it>
2016-03-13 19:39:15 +0100

    added jaccard similarity multiset

--- Similarity/similarityFunction.py
+++ Similarity/similarityFunction.py
@@ -1,10 +1,30 @@
 def jaccardSimilarity(set1, set2):
     inter = set.intersection(set1, set2)
     union = set.union(set1, set2)
-    print(inter, union)
     return (len(inter)/len(union))
+
+
+def returnDict(list1):
+    dict1 = dict()
+    for el in list1:
+        if el not in dict1:
+            dict1[el] = 1
+        else:
+            dict1[el] += 1
+    return dict1
+
+
+def jaccardSimilarityWithMultiSet(list1, list2):
+    dict1 = returnDict(list1)
+    dict2 = returnDict(list2)
+    inter = sum([min(value, dict2[key]) for key, value in dict1.items() if key in dict2])
+    union = len(list1)+len(list2)
+    return (inter/union)
 
 if __name__ == '__main__':
     set1 = {1, 2, 3, 5, 7, 10}
     set2 = {2, 3, 7, 10, 11}
     print(jaccardSimilarity(set1, set2))
+    list1 = ['a', 'a', 'a', 'b']
+    list2 = ['a', 'a', 'b', 'b', 'c']
+    print(jaccardSimilarityWithMultiSet(list1, list2))
